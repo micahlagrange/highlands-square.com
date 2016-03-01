de941ff5126daee3d37311506676dd9997a521ff
Micah Turner <turner.micah@gmail.com>
2016-02-29 23:20:24 -0700

    updated deploy ruby script, updated events page

--- hsma_deploy.sh
+++ hsma_deploy.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+set -e
 ruby deployment_scripts/djangoDeploy.rb code hisquare deploy hsma01p /home/deploy/hsma_app
--- hsma_staticdeploy.sh
+++ hsma_staticdeploy.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+set -e
+
 #sudo ruby djangoDeploy.rb staticfiles /usr/local/highlands-square.com/static deploy hsma01p /www/hisquare/static
+
 rsync -a --no-o --no-p /usr/local/highlands-square.com/static/* hsma01p:/www/hisquare/static
