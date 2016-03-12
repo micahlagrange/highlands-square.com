bcc2219a896ba00fe706455c46843e26a6fa942b
Micah Turner <turner.micah@gmail.com>
2016-03-12 11:19:37 -0700

    updated static deploy script

--- hsma_staticdeploy.sh
+++ hsma_staticdeploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+# Simply sync static files directory with remote server.
 
 set -e
 
-#sudo ruby djangoDeploy.rb staticfiles /usr/local/highlands-square.com/static deploy hsma01p /www/hisquare/static
-
-rsync -a --no-o --no-p /usr/local/highlands-square.com/static/* hsma01p:/www/hisquare/static
+rsync -az --no-o --no-p highsquare/home/static/ hsma01p:/www/hisquare/static/
